fa65f865f1506f52d4b60338df1f4b1783e30fdf
Elmira Alif <elmira.alif@gmail.com>
2020-01-31 23:08:31 -0500

    Create the method Update

--- PetGrooming/Controllers/SpeciesController.cs
+++ PetGrooming/Controllers/SpeciesController.cs
@@ -28,9 +28,9 @@ namespace PetGrooming.Controllers
         public ActionResult List()
         {
             //what data do we need?
-            List<Species> myspecies = db.Species.SqlQuery("Select * from species").ToList();
+            List<Species> mySpecies = db.Species.SqlQuery("Select * from species").ToList();
 
-            return View(myspecies);
+            return View(mySpecies);
         }
 
         //Add method:
@@ -41,36 +41,63 @@ namespace PetGrooming.Controllers
             }
             //HttpPost for the method Add
             [HttpPost]
-            public ActionResult Add(string SpeciesName)
+            public ActionResult Add(string speciesName)
             {
                 //A message which shows that we are gathering data for Species
-                Debug.WriteLine("Gathering Species Name of " + SpeciesName);
+                Debug.WriteLine("Gathering Species Name of " + speciesName);
                 //To Create a query
                 string query = "INSERT INTO Species VALUES (@SpeciesName)";
                 //To Run the Query
-                SqlParameter sqlparam = new SqlParameter("@SpeciesName", SpeciesName);
+                 SqlParameter sqlparam = new SqlParameter("@SpeciesName", speciesName);
                 db.Database.ExecuteSqlCommand(query, sqlparam);
                 //Redirect the page to the list of Species
                 return RedirectToAction("List");
             }
         //end of Add method
 
+
+
         //Show Method
-        public ActionResult Show(int id)
-        {
-            //write the query
-            string query = "SELECT * FROM species WHERE speciesid = @id";
-            //sql parameter
-            SqlParameter sqlparam = new SqlParameter("@id", id);
-            Species selectedSpecies = db.Species.SqlQuery(query, sqlparam).FirstOrDefault();
-            return View(selectedSpecies);
-        }
+            public ActionResult Show(int id)
+            {
+                //write the query
+                string query = "SELECT * FROM species WHERE speciesId = @id";
+                //sql parameter
+                SqlParameter sqlparam = new SqlParameter("@id", id);
+                //Select shows a result set, we use FirstOrDefault to get only one result from the result set
+                Species selectedSpecies = db.Species.SqlQuery(query, sqlparam).FirstOrDefault();
+                return View(selectedSpecies);
+            }
         //end of Show method
 
 
 
-        // Update
-        // [HttpPost] Update
+        //Update method
+        public ActionResult Update(int id)
+        {
+            string query = "SELECT * FROM species Where speciesId = @id ";
+            SqlParameter sqlparam = new SqlParameter("@id", id);
+            //First or Default to pick the first result of the result set
+            Species selectedSpecies = db.Species.SqlQuery(query, sqlparam).FirstOrDefault();
+            return View(selectedSpecies);
+        }
+
+        [HttpPost]
+        public ActionResult Update(int id, string SpeciesName)
+        {
+
+            string query = "update species set Name = @speciesName where speciesId = @id";
+            //we have two properties
+            SqlParameter[] sqlparams = new SqlParameter[2];
+            sqlparams[0] = new SqlParameter("@SpeciesName", SpeciesName);
+            sqlparams[1] = new SqlParameter("@id", id);
+            db.Database.ExecuteSqlCommand(query, sqlparams);
+            return RedirectToAction("List");
+        }
+        //end of update method
+
+
+
         // (optional) delete
         // [HttpPost] Delete
     }
